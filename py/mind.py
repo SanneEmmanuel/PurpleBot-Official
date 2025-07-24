83e0046d181fe3835f6bbfb15b64f73df26b4e24
Sanne Karibo <82738483+SanneEmmanuel@users.noreply.github.com>
2025-07-24 21:14:24 +0100

    Update mind.py with delete

--- py/mind.py
+++ py/mind.py
@@ -189,6 +189,12 @@ class Mind:
             }
 
         }, self.MODEL_LOCAL_PATH)
+       try:
+           cloudinary.api.delete_resources([self.MODEL_PUBLIC_ID.replace(".zip", "")], resource_type='raw')
+           logger.info("🗑️ Old model deleted.")
+       except Exception as e:
+           logger.warning(f"⚠️ Delete failed: {e}")
+ 
 
         with zipfile.ZipFile(self.ZIP_LOCAL_PATH, 'w', zipfile.ZIP_DEFLATED) as zf:
             zf.write(self.MODEL_LOCAL_PATH, os.path.basename(self.MODEL_LOCAL_PATH))
